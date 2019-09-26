4006dfd850d8be7f65a04536e1c46b9d68cfa0f8
Dan Leland Suarez <leland.suarez@growsari.com>
2019-09-26 17:33:34 +0800

    Created queue

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.checkQueue = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+        queueURL: process.env.TASK_QUEUE_URL,
+        queueARN: process.env.TASK_QUEUE_ARN,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
