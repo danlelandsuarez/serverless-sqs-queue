e75a42b7e17e4d8ef8ae1bd292f315abc848108d
Dan Leland Suarez <leland.suarez@growsari.com>
2019-09-30 22:50:27 +0800

    Working consumer

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.checkQueue = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Checking env variables...',
         input: event,
         queueURL: process.env.TASK_QUEUE_URL,
         queueARN: process.env.TASK_QUEUE_ARN,
@@ -14,7 +14,46 @@ module.exports.checkQueue = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.consume = async event => {
+  for (const record of event.Records) {
+    console.log("Processing record ", JSON.stringify(record))
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      event,
+      null,
+      2
+    )
+  }
+}
+
+const AWS = require('aws-sdk');
+
+module.exports.newMessage = async event => {
+  const sqs = new AWS.SQS()
+  const payload = {
+    DelaySeconds: 10,
+    MessageAttributes: {
+      "SomeAttribute": {
+        DataType: "String",
+        StringValue: "Some Value"
+      }
+    },
+    MessageBody: JSON.stringify(event.body),
+    QueueUrl: process.env.TASK_QUEUE_URL
+  }
+
+  await sqs.sendMessage(payload).promise()
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      { message: "queued" },
+      null,
+      2
+    )
+  }
+}
